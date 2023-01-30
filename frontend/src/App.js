4e02c0124a9b4e38596c04f70a93e942a8092ef0
Vasco Afonso <vascoafonso@MS7B984f0efad3.Home>
2023-01-30 15:23:18 +0000

    Added Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import MainContainer from './components/MainContainer';
+import { Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainContainer />}></Route>
+      <Route path="/" element={<h1>Home</h1>}></Route>
       <Route path="/about" element={<h1>About</h1>}></Route>
       <Route path="/contact" element={<h1>Contacts</h1>}></Route>
     </Routes>
