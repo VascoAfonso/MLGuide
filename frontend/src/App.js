1237ae24f4b1b68327cd4ce8f99795d90b2c2122
Vasco Afonso <vascoafonso@MS7B984f0efad3.Home>
2023-01-30 14:33:05 +0000

    Added Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import MainContainer from './components/MainContainer'
+import MainContainer from './components/MainContainer';
 
 function App() {
   return (
