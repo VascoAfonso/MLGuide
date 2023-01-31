6401482f4b7c8f7b6ca4132b8d6c27577374fc91
Vasco Afonso <vascoafonso@MS7B984f0efad3.Home>
2023-01-31 13:44:28 +0000

    Added element Main Container to Home Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes} from 'react-router-dom';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>}></Route>
+      <Route path="/" element={<MainContainer />}></Route>
       <Route path="/about" element={<h1>About</h1>}></Route>
       <Route path="/contact" element={<h1>Contacts</h1>}></Route>
     </Routes>
