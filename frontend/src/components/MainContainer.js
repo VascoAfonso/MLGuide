b42177de83b01d72402a968bdf1227c8e61525e1
joaovaz22 <vaz.joao2002@gmail.com>
2023-02-01 23:08:08 +0000

    Perfecting home page

--- frontend/src/components/MainContainer.js
+++ frontend/src/components/MainContainer.js
@@ -1,20 +1,24 @@
 import './MainContainer.css'
 import Item from './Item.js'
+import lazyLearning from '../images/LazyLearning.png';
+import decisionTree from '../images/DecisionTree.png';
+import regression from '../images/Regression.png';
+import clustering from '../images/Clustering.png';
 const MainContainer = () => {
     return (
         <div className="MainContainer">
             <div className="MainContainer-row">
-                <Item text='Decision Tree' />
+                <Item text='Decision Tree' image={decisionTree} alt={'DecisionTree'} />
                 <Item text='Bayesian Learning' />
-                <Item text={'Lazy Learning'} />
+                <Item text={'Lazy Learning'} image={lazyLearning} alt={'LazyLearning'} />
             </div>
             <div className="MainContainer-row">
-                <Item text="Regression" />
+                <Item text="Regression" image={regression} alt={'regression'} />
                 <Item text='Perceptron' />
                 <Item text='MultiLayer Perceptron' />
             </div>
             <div className="MainContainer-row">
-                <Item text='Clustering' />
+                <Item text='Clustering' image={clustering} alt={'clustering'} />
             </div>
         </div>
     );
